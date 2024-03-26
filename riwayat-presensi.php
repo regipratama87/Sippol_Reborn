1d39058f71120384d6797e24dec05fcebf14ce94
regipratama87 <76906577+regipratama87@users.noreply.github.com>
2024-03-26 20:16:13 +0700

    Update riwayat-presensi.php

--- riwayat-presensi.php
+++ riwayat-presensi.php
@@ -35,6 +35,7 @@ if (!isset($_SESSION['login_user'])) {
     <meta name="author" content="">
     <title>Riwayat Presensi - <?php echo $title ?></title>
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+		<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
@@ -156,12 +157,11 @@ mysqli_close($koneksi);
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="js/sb-admin-2.min.js"></script>
     
-		<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-		<script src="js/sb-admin-2.min.js"></script>
 		<script>
 			
 			$(document).ready(function() {
